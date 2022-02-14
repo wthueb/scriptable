e2026c6dca1cb63f7f942a8bcd8672f0d9e988d2
William Huebner <wilhueb@gmail.com>
2022-02-14 14:25:09 -0500

    addTrack instead of add_track

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 
-cat > ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/add\ track.js <<- EOM
+cat > ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/addTrack.js <<- EOM
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-brown; icon-glyph: magic;
 EOM
 
-cat dist/add_track.js >> ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/add\ track.js
+cat dist/addTrack.js >> ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/addTrack.js
